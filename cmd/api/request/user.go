74196bc40823a6977a1b0375e3ec0684b74b6db4
alitdarmaputra <alitdarmaputra@gmail.com>
2023-02-13 14:36:54 +0800

    fix: user update request validator

--- cmd/api/request/user.go
+++ cmd/api/request/user.go
@@ -20,8 +20,8 @@ type UserUpdateRequest struct {
 	Address     string  `json:"address"      binding:"required"`
 	AreaId      int     `json:"area_id"      binding:"required,numeric"`
 	RoleId      int8    `json:"role_id"      binding:"required,numeric"`
-	Latitude    float64 `json:"latitude"     binding:"required,geolocation"`
-	Longitude   float64 `json:"longitude"    binding:"required,geolocation"`
+	Latitude    float64 `json:"latitude"     binding:"required,latitude"`
+	Longitude   float64 `json:"longitude"    binding:"required,longitude"`
 }
 
 type UserLoginRequest struct {
