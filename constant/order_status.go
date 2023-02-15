7bd56ec60d3cb05a05d4a46b039e3a3413125c45
alitdarmaputra <alitdarmaputra@gmail.com>
2023-02-15 16:48:39 +0800

    fix: order status message

--- constant/order_status.go
+++ constant/order_status.go
@@ -1,8 +1,8 @@
 package constant
 
 const (
-	StatusProcess   = "status-process"
-	StatusDelivered = "status-delivered"
-	StatusRejected  = "status-rejected"
-	StatusCanceled  = "status-canceled"
+	StatusProcess   = "PROCESS"
+	StatusDelivered = "DELIVERED"
+	StatusRejected  = "REJECTED"
+	StatusCanceled  = "CANCELED"
 )
